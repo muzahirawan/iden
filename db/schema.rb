0604e73c7264e5410818d3f192df65d861ead6a7
muzahirawan <muzahirawan@gmail.com>
2023-04-12 13:24:39 +0500

    2nd commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_111612) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_082158) do
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
